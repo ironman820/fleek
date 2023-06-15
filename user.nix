34c4706d8efb8fca6c59596392841b71a8bbb812
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-06-15 13:59:39 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -1,22 +1,22 @@
 { pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
-  programs.bash.shellAliases = {
-	  ca="chezmoi add";
-	  ce="chezmoi edit --apply";
-	  cu="chezmoi update";
-	  cc="chezmoi cd";
-	  cf="chezmoi forget";
-	  ci="chezmoi init";
-	  cr="chezmoi re-add";
-    df = "duf";
-    ducks = "du -chs * 2>/dev/null | sort -rh | head -11";
-    htop = "glances";
-    pdo="podman images | awk '{print \$3,\$2}' | grep '<none>' | awk '{print \$1}' | xargs -t podman rmi ";
-    pdi="podman images";
-    pdr="podman rmi";
+  programs.bash = {
+    sessionVariables.EDITOR = "vim";
+    shellAliases = {
+      ca="chezmoi add";
+      ce="chezmoi edit --apply";
+      cu="chezmoi update";
+      cc="chezmoi cd";
+      cf="chezmoi forget";
+      ci="chezmoi init";
+      cr="chezmoi re-add";
+      df = "duf";
+      ducks = "du -chs * 2>/dev/null | sort -rh | head -11";
+      htop = "glances";
+      pdo="podman images | awk '{print \$3,\$2}' | grep '<none>' | awk '{print \$1}' | xargs -t podman rmi ";
+      pdi="podman images";
+      pdr="podman rmi";
+    };
   };
  
 }
