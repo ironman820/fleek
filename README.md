a5931efe0efb2fed51e8ebead314b76b54f09c1a
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-06-15 16:47:52 -0500

    fleek: apply

--- README.md
+++ README.md
@@ -19,4 +19,30 @@ $ apply-$(hostname)
 ```
 
 Your actual aliases are listed below:
+    ca = "chezmoi add";
+
+    cc = "chezmoi cd";
+
+    ce = "chezmoi edit --apply";
+
+    cf = "chezmoi forget";
+
+    ci = "chezmoi init";
+
+    cr = "chezmoi re-add";
+
+    cu = "chezmoi update";
+
+    df = "duf";
+
+    ducks = "du -chs * 2>/dev/null | sort -rh | head -11";
+
     fleeks = "cd ~/.local/share/fleek";
+
+    htop = "glances";
+
+    pdi = "podman images";
+
+    pdo = "podman images | awk '{print \$3,\$2}' | grep '<none>' | awk '{print \$1}' | xargs -t podman rmi";
+
+    pdr = "podman rmi";
--- aliases.nix
+++ aliases.nix
@@ -1,7 +1,33 @@
 { pkgs, misc, ... }: {
   # DO NOT EDIT: This file is managed by fleek. Manual changes will be overwritten.
    home.shellAliases = {
+    "ca" = "chezmoi add";
+    
+    "cc" = "chezmoi cd";
+    
+    "ce" = "chezmoi edit --apply";
+    
+    "cf" = "chezmoi forget";
+    
+    "ci" = "chezmoi init";
+    
+    "cr" = "chezmoi re-add";
+    
+    "cu" = "chezmoi update";
+    
+    "df" = "duf";
+    
+    "ducks" = "du -chs * 2>/dev/null | sort -rh | head -11";
+    
     "fleeks" = "cd ~/.local/share/fleek";
+    
+    "htop" = "glances";
+    
+    "pdi" = "podman images";
+    
+    "pdo" = "podman images | awk '{print \$3,\$2}' | grep '<none>' | awk '{print \$1}' | xargs -t podman rmi";
+    
+    "pdr" = "podman rmi";
     
     # bat --plain for unformatted cat
     catp = "bat -P";
