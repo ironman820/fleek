d3925453a7791c18185b4a150fc91aae36e53948
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-06-17 23:05:47 -0500

    fleek: apply

--- aliases.nix
+++ aliases.nix
@@ -32,11 +32,5 @@
     "pdo" = "podman images | awk '{print \$3,\$2}' | grep '<none>' | awk '{print \$1}' | xargs -t podman rmi";
     
     "pdr" = "podman rmi";
-    
-    # bat --plain for unformatted cat
-    catp = "bat -P";
-    
-    # replace cat with bat
-    cat = "bat";
     };
 }
--- home.nix
+++ home.nix
@@ -29,14 +29,6 @@
     pkgs.htop
     pkgs.github-cli
     pkgs.glab
-    pkgs.fzf
-    pkgs.ripgrep
-    pkgs.lazygit
-    pkgs.jq
-    pkgs.yq
-    pkgs.neofetch
-    pkgs.btop
-    pkgs.cheat
     (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
   ];
   fonts.fontconfig.enable = true; 
