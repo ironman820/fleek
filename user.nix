e005701c9e830f6300a035919081a86f4718962e
Nicholas Eastman <29488820+ironman820@users.noreply.github.com>
2023-06-21 19:30:02 -0500

    fleek: apply

--- user.nix
+++ user.nix
@@ -20,4 +20,10 @@
     enable = true;
     tray.enable = true;
   };
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = [ "graphical-session-pre.target" ];
+    };
+  };
 }
